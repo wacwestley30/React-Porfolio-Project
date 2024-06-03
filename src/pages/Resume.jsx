8535159e8b46376dda4468e5e3174babddb5e32e
wacwestley30 <westleycervantes@gmail.com>
2024-06-03 07:13:09 -0400

    Updated Resume to better view the resume and download link for the pdf. Just need to use a color scheme on entire site.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
@@ -7,18 +6,23 @@ import myResume from '../assets/SoftwareDeveloperResume.pdf';
 
 export default function Resume() {
     return (
-        <div>
-            <h1>Resume</h1>
+        <div className="container">
+            <h1 className="my-4">Resume</h1>
             <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}>
                 <div
                     style={{
                         border: '1px solid rgba(0, 0, 0, 0.3)',
-                        height: '750px',
+                        height: '50rem',
                     }}
                 >
                     <Viewer fileUrl={myResume} />
                 </div>
             </Worker>
+            <div className="pt-3">
+                <a href={myResume} download="SoftwareDeveloperResume.pdf" className="text-dark">
+                    Download Resume
+                </a>
+            </div>
         </div>
     );
 }
