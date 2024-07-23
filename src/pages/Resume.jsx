a7cb868fc9a3010f6c1d171a700d7ebb0148ecf5
wacwestley30 <westleycervantes@gmail.com>
2024-07-23 06:49:22 -0400

    updated Resume for better Version Control of React-Pdf-Viewer

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,14 +1,17 @@
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
+import packageJson from '../../package.json';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import myResume from '/assets/SoftwareDeveloperResume.pdf';
+
+const pdfjsVersion = packageJson.dependencies['pdfjs-dist'];
 
 export default function Resume() {
     return (
         <div className="container">
             <h1 className="my-4">Resume</h1>
-            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`}>
+            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}>
                 <div
                     style={{
                         border: '1px solid rgba(0, 0, 0, 0.3)',
